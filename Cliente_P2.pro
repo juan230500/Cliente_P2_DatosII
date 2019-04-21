5e23069d94ff42ec7f65b7f364445d13fa6b41e2
Erick B <erick.cr12@outlook.com>
2019-04-21 16:44:07 -0600

    Creación de ventanaTablero(Zona de intimidación)
    1. Se creó una ventana que contiene un frame, donde se crea el tablero.
    2. Se creó una matriz 10x10 de widgets, con todas las casillas en rojo, indicando que son casillas libres.
    3. Se puede crear diferentes tipos de obstáculos, en posiciones indicadas por una matriz de enteros ([[i1,j1],[i2,j2],...,[in,jn]].
    4. Se visualiza una ruta específicada por un string con el formato: "i1j1-i2j2-...-injn"

--- Cliente_P2.pro
+++ Cliente_P2.pro
@@ -26,13 +26,16 @@ CONFIG += c++11
 
 SOURCES += \
         main.cpp \
-        mainwindow.cpp
+        mainwindow.cpp \
+        ventanatablero.cpp
 
 HEADERS += \
-        mainwindow.h
+        mainwindow.h \
+        ventanatablero.h
 
 FORMS += \
-        mainwindow.ui
+        mainwindow.ui \
+        ventanatablero.ui
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
---
+++ ventanatablero.h
@@ -0,0 +1,45 @@
+#ifndef VENTANATABLERO_H
+#define VENTANATABLERO_H
+
+#include <QDialog>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QVBoxLayout>
+#include <QGraphicsRectItem>
+#include <QTextStream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+#include <boost/algorithm/string.hpp>
+
+namespace Ui {
+class VentanaTablero;
+}
+
+class VentanaTablero : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit VentanaTablero(QWidget *parent = 0);
+    void inicializarScene();
+    void inicializarView();
+    void generarTablero();
+    void posicionarCasilla(int xPos, int yPos, int valorTablero);
+    void pintarCasilla(QGraphicsRectItem* rItem, Qt::GlobalColor color, std::vector<QGraphicsRectItem*> vectorWidgets);
+    void posicionarObstaculos(int obstaculos[][3], int largoArray);
+    void mostrarRuta(std::string ruta);
+    void eliminarCasillas(std::vector<QGraphicsRectItem*> widgets);
+    ~VentanaTablero();
+
+private:
+    Ui::VentanaTablero* ui;
+    QGraphicsScene* scene;
+    QGraphicsView* view;
+    QVBoxLayout* layoutVertical;
+    std::vector<QGraphicsRectItem*> rutaWidgets;
+    std::vector<QGraphicsRectItem*> obstaculosWidgets;
+};
+
+#endif // VENTANATABLERO_H
